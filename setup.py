a2bdcd556cf7744bd1e501258ec04230e3f410d6
Nancy Tao <Nancy.Tao@turner.com>
2018-04-17 09:09:05 -0400

    Add setup.py for pip installability

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='aws-interesting-api-calls',
+    version='0.1',
+    description='Module inventorying AWS API calls of significance to security',
+    url='https://github.com/jchrisfarris/aws-interesting-api-calls',
+    author='Chris Farris',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False)
