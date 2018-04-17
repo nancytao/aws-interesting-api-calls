f6c121ea4b6e4aaa6aa3d747c21aa68ba35c9a78
Nancy Tao <Nancy.Tao@turner.com>
2018-04-17 10:24:47 -0400

    Fix module name

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-setup(name='aws-interesting-api-calls',
+setup(name='aws_interesting_api_calls',
     version='0.1',
     description='Module inventorying AWS API calls of significance to security',
     url='https://github.com/jchrisfarris/aws-interesting-api-calls',
